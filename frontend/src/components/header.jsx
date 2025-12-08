5cc64f73ab814cd5b7b7521385d9bd641fdc3e3e
Gabriel Cordeiro <gabrielamorimcordeiro4321@gmail.com>
2025-12-07 20:28:19 -0400

    Fixed Navbar Width

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,7 +1,7 @@
 export function Navbar() {
     return(
-        <div class="containerd d-block"> 
-            <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom"> 
+        <div class="containerd d-block w-100"> 
+            <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom w-auto"> 
                 <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"> 
                 <svg class="bi me-2" width="40" height="32" aria-hidden="true">
                     <use xlink:href="#bootstrap"></use>
